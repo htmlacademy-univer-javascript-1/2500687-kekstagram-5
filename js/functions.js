0de32584322f4e22d462fcd0384e2487e4f5845e
Илья <ilaslinkov@gmail.com>
2024-09-23 02:47:53 +0500

    исправит функции

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,14 @@
 function checkStringLength(string, length) {
   return (string.length <= length);
 }
+checkStringLength();
 
 function isPalindrome (string) {
   const normalizedString = string.replaceAll(' ', '').toLowerCase();
   let reversedString = '';
   for (let i = normalizedString.length - 1; i >= 0; i--) {
-    let curSymbol = normalizedString[i];
-    reversedString += curSymbol;
+    reversedString += normalizedString[i];
   }
   return (reversedString === normalizedString);
 }
+isPalindrome();
